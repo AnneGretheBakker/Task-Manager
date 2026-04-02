ecba85c5fce9e162b6034d887853b87b32f250f1
Anne Bakker <anne.bakker3@hva.nl>
2026-04-02 15:59:54 +0200

    Fix: Path to task now to task index

--- src/Security/AppLoginAuthenticator.php
+++ src/Security/AppLoginAuthenticator.php
@@ -48,7 +48,7 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('/task'));
+        return new RedirectResponse($this->urlGenerator->generate('app_task_index'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
